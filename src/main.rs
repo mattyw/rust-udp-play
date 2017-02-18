bf6937c199715312105b4a0120ee3816b15834f4
mattyw <gh@mattyw.net>
2017-02-18 21:36:17 +0000

    an echoing udp server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::thread;
+use std::net;
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 
@@ -6,25 +7,39 @@ fn foo(a: &mut i32) -> i32 {
     *a = *a + 1;
     *a + 1
 }
+
+fn socket(listen_on: net::SocketAddr) -> net::UdpSocket {
+    let attempt = net::UdpSocket::bind(listen_on);
+    let mut socket;
+    match attempt {
+        Ok(sock) => {
+            println!("bound to {}", listen_on);
+            socket = sock;
+        }
+        Err(err) => panic!("Could not bind {}", err),
+    };
+    socket
+}
+
+fn read_message(socket: &net::UdpSocket) {
+    let mut buf: [u8; 1] = [0; 1];
+    let result = socket.recv_from(&mut buf);
+    match socket.send(&mut buf) {
+        Ok(_) => println!("sent"),
+        Err(err) => println!("failed to send {}", err),
+    }
+    drop(socket);
+}
+
+fn listen(listen_on: net::SocketAddr) {
+    let sock = socket(listen_on);
+    loop {
+        read_message(&sock);
+    }
+}
+
 fn main() {
-    let threads = 100;
-    let x = &mut 42;
-    let y = foo(x);
-    println!("Hello, world! {} {}", x, y);
-    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
-    for i in 0..threads {
-        let tx_t = tx.clone();
-        thread::spawn(move || {
-            tx_t.send(i).unwrap();
-            println!("this is thread number {}", i)
-        });
-    }
-    for _ in 0..threads {
-        let x = match rx.recv() {
-            Ok(num) => num,
-            Err(_) => -1,
-        };
-        println!("{}", x);
-    }
-    println!("done")
+    let ip = net::Ipv4Addr::new(127, 0, 0, 1);
+    let listen_addr = net::SocketAddrV4::new(ip, 4444);
+    listen(net::SocketAddr::V4(listen_addr));;
 }
