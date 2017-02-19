0adcf9c2b65f1ad7c3d2ad2e317420338960483c
mattyw <gh@mattyw.net>
2017-02-19 23:09:02 +0000

    a very crude pair of echo udp servers

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@ use std::net;
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 
-fn foo(a: &mut i32) -> i32 {
-    *a = *a + 1;
-    *a + 1
-}
-
 fn socket(listen_on: net::SocketAddr) -> net::UdpSocket {
     let attempt = net::UdpSocket::bind(listen_on);
     let mut socket;
@@ -21,25 +16,55 @@ fn socket(listen_on: net::SocketAddr) -> net::UdpSocket {
     socket
 }
 
-fn read_message(socket: &net::UdpSocket) {
-    let mut buf: [u8; 1] = [0; 1];
-    let result = socket.recv_from(&mut buf);
-    match socket.send(&mut buf) {
-        Ok(_) => println!("sent"),
-        Err(err) => println!("failed to send {}", err),
-    }
-    drop(socket);
+fn read_message(server_name: &str, socket: &net::UdpSocket) {
+    let mut buf: [u8; 10] = [0; 10];
+    match socket.recv_from(&mut buf) {
+        Err(err) => {
+            println!("failed to read {}", err);
+        }
+        Ok((x, addr)) => {
+            println!("{} got {:?}",
+                     server_name,
+                     String::from_utf8_lossy(&buf[0..x]));
+            match socket.send_to(&mut &buf[0..x], addr) {
+                Ok(_) => println!("sent"),
+                Err(err) => println!("failed to send {}", err),
+            }
+        }
+    };
 }
 
-fn listen(listen_on: net::SocketAddr) {
-    let sock = socket(listen_on);
-    loop {
-        read_message(&sock);
+fn listen(server_name: &str, sock: net::UdpSocket, listen_on: net::SocketAddr) {
+    for i in 0..50 {
+        read_message(server_name, &sock);
     }
+}
+
+fn send(sock: &net::UdpSocket, addr: net::SocketAddr, msg: &[u8]) {
+    sock.send_to(&msg, addr);
+}
+
+fn server(server_name: &str,
+          server_addr: net::SocketAddr,
+          other_addr: net::SocketAddr,
+          msg: &[u8]) {
+    let sock = socket(server_addr);
+    send(&sock, other_addr, &msg);
+    listen(server_name, sock, server_addr);
+    drop(socket);
 }
 
 fn main() {
     let ip = net::Ipv4Addr::new(127, 0, 0, 1);
-    let listen_addr = net::SocketAddrV4::new(ip, 4444);
-    listen(net::SocketAddr::V4(listen_addr));;
+    let server_listen = net::SocketAddrV4::new(ip, 4444);
+    let client_listen = net::SocketAddrV4::new(ip, 5555);
+    let server_addr = net::SocketAddr::V4(server_listen);
+    let client_addr = net::SocketAddr::V4(client_listen);
+    let msg_a: &[u8] = b"msg A";
+    let msg_b: &[u8] = b"msg B";
+
+    let t0 = thread::spawn(move || server("A", server_addr, client_addr, msg_a));
+    let t1 = thread::spawn(move || server("B", client_addr, server_addr, msg_b));
+    let _ = t0.join();
+    let _ = t1.join();
 }
